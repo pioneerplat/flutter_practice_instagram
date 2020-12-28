de1b619b27afef4530bff77f4e1b42604a4deba0
pioneerplat <pioneerplat@gmail.com>
2020-12-29 00:24:08 +0900

    11 feed screen에 Listview

--- flutter_instagram_first/lib/feed_screen.dart
+++ flutter_instagram_first/lib/feed_screen.dart
@@ -8,8 +8,16 @@ class FeedScreen extends StatelessWidget {
         title: Text('instagram',
           style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black87),
         ),
-
       ),
+      body: ListView.builder(itemBuilder: feedListBuilder, itemCount: 30,),
+    );
+  }
+
+  Widget feedListBuilder(BuildContext context, int index) {
+    return Container(
+      //accents color를 인덱스의 수만큼 받아와서 하나씩 번갈아 가면서 사용한다는 뜻
+      color: Colors.accents[index % Colors.accents.length],
+      height: 100,
     );
   }
 }
