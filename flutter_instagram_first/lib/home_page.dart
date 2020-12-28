5599434abd1565ccbd9008fb9ee40634b5f8b1bf
pioneerplat <pioneerplat@gmail.com>
2020-12-28 20:30:12 +0900

    6 bottom navigation bar 생성

--- flutter_instagram_first/lib/home_page.dart
+++ flutter_instagram_first/lib/home_page.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({
+class HomePage extends StatefulWidget {
+  HomePage({
     Key key,
   }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<BottomNavigationBarItem> btmNavItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.home), label: " "),
+    BottomNavigationBarItem(icon: Icon(Icons.search), label: " "),
+    BottomNavigationBarItem(icon: Icon(Icons.add), label: " "),
+    BottomNavigationBarItem(icon: Icon(Icons.healing), label: " "),
+    BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: " ")
+  ];
+
+  int _selectedIntex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +28,27 @@ class HomePage extends StatelessWidget {
         ),
         body: Container(
           color: Colors.green,
-        )
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed, // 아이탬이 선택 되었을때 움직이지 않게 고정
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: btmNavItems,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: Colors.black,
+          currentIndex: _selectedIntex,
+          onTap: _onBtmItemClick,
+        ),
     );
   }
+
+  void _onBtmItemClick(int index) {
+    print(index);
+    setState(() {
+      _selectedIntex = index;
+    });
+
+  }
 }
+
 
