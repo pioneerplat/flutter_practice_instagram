8f417b476e7ab378041fdcf825966dbe36293feb
pioneerplat <pioneerplat@gmail.com>
2020-12-29 01:38:57 +0900

    12 appbar(actionbar) 만들기
    Column, Row, ListView

--- flutter_instagram_first/lib/feed_screen.dart
+++ flutter_instagram_first/lib/feed_screen.dart
@@ -1,14 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      /*
       appBar: AppBar(
         title: Text('instagram',
           style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black87),
         ),
       ),
+      */
+      appBar: CupertinoNavigationBar(
+
+        // 왼쪽
+        leading: IconButton(
+            //쿠퍼티노 디자인 사용법
+            icon: Icon(CupertinoIcons.photo_camera_solid, color: Colors.black87,),
+            // Material 디자인
+            //icon: Icon(Icons.camera_alt),
+            onPressed: null
+        ),
+
+        // 가운데
+          middle: Text('instagram',
+          style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black87),
+        ),
+
+        // 오른쪽
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/actionbar_camera.png'),
+                    color: Colors.black87,
+                  ),
+                  onPressed: null),
+              IconButton(
+                  icon: ImageIcon(
+                    AssetImage('assets/images/actionbar_camera.png'),
+                    color: Colors.black87,
+                  ),
+                  onPressed: null)
+            ],
+
+          ),
+      ),
+
       body: ListView.builder(itemBuilder: feedListBuilder, itemCount: 30,),
     );
   }
