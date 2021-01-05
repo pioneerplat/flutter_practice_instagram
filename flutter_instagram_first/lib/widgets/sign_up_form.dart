77e4e4bead10eb412bcebed1693fa2409803d532
pioneerplat <pioneerplat@gmail.com>
2021-01-05 18:46:36 +0900

    39 SignUpForm layout

--- flutter_instagram_first/lib/widgets/sign_up_form.dart
+++ flutter_instagram_first/lib/widgets/sign_up_form.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  //formkey란 form위젯의 상태를 저장하는 key
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black45,
+    return Form(
+      //스크롤이 가능한 ListView를 넣어줌
+      key: _formkey,
+      child: ListView(
+        children: [
+          TextFormField(
+
+          ),
+          TextFormField(
+
+          ),
+          TextFormField(
+
+          ),
+        ],
+      ),
     );
   }
 }
