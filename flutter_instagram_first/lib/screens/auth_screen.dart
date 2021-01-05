150b7669c2b2ad61c0bd2694d6629aeca610d77b
pioneerplat <pioneerplat@gmail.com>
2021-01-06 01:47:27 +0900

    45 keyboard에 따른 화면 조절

--- flutter_instagram_first/lib/screens/auth_screen.dart
+++ flutter_instagram_first/lib/screens/auth_screen.dart
@@ -16,50 +16,55 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //true로 되어있으면 키보드 같은게 올라오면서 화면크기를 줄여준다. 화면의 변화를 주지않기 위해 false
+        resizeToAvoidBottomInset: false,
         body: SafeArea(
             child: Stack(
-      children: <Widget>[
-        FadeStack(
-          selectedForm: selectedForm,
-        ),
-        //Positioned Stack안에서만 사용 가능
-        Positioned(
-          left: 0,
-          right: 0,
-          bottom: 0,
-          height: 40,
-          child: Container(
-            color: Colors.white,
-            child: FlatButton(
-              shape: Border(top: BorderSide(color: Colors.grey)),
-              onPressed: () {
-                setState(() {
-                  //if( <instance> is <class>){} 타입비교
-                  if (selectedForm == 0) {
-                    selectedForm = 1;
-                  } else {
-                    selectedForm = 0;
-                  }
-                });
-              },
-              child: RichText(
-                text: TextSpan(
-                  text: (selectedForm == 0)
-                      ? "Already have an account? "
-                      : "Don't have an account? ",
-                  style: TextStyle(color: Colors.grey),
-                  children: [
-                    TextSpan(
-                        text: (selectedForm == 0) ? "Sign In" : "Sign Up",
-                        style: TextStyle(
-                            color: Colors.blue, fontWeight: FontWeight.bold)),
-                  ],
+              children: <Widget>[
+                FadeStack(
+                  selectedForm: selectedForm,
                 ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    )));
+                //Positioned Stack안에서만 사용 가능
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  height: 40,
+                  child: Container(
+                    color: Colors.white,
+                    child: FlatButton(
+                      shape: Border(top: BorderSide(color: Colors.grey)),
+                      onPressed: () {
+                        setState(() {
+                          //if( <instance> is <class>){} 타입비교
+                          if (selectedForm == 0) {
+                            selectedForm = 1;
+                          } else {
+                            selectedForm = 0;
+                          }
+                        });
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          text: (selectedForm == 0)
+                              ? "Already have an account? "
+                              : "Don't have an account? ",
+                          style: TextStyle(color: Colors.grey),
+                          children: [
+                            TextSpan(
+                                text: (selectedForm == 0)
+                                    ? "Sign In"
+                                    : "Sign Up",
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )));
   }
 }
--- flutter_instagram_first/lib/widgets/sign_up_form.dart
+++ flutter_instagram_first/lib/widgets/sign_up_form.dart
@@ -25,75 +25,80 @@ class _SignUpFormState extends State<SignUpForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(common_gap),
-      child: Form(
-        //스크롤이 가능한 ListView를 넣어줌
-        key: _formkey,
-        child: ListView(
-          children: [
-            SizedBox(
-              height: common_l_gap,
-            ),
-            Image.asset('assets/images/insta_text_logo.png'),
-            TextFormField(
-              controller: _emailController,
-              cursorColor: Colors.black54,
-              decoration: _textInputDecor('Email'),
-              validator: (text) {
-                if (text.isNotEmpty && text.contains("@")) {
-                  return null;
-                } else {
-                  return '정확한 이메일을 주소를 입력해주세요.';
-                }
-              },
-            ),
-            SizedBox(height: common_xs_gap),
-            TextFormField(
-              controller: _pwController,
-              cursorColor: Colors.black54,
-              //비밀번호 암호화
-              obscureText: true,
-              decoration: _textInputDecor('Password'),
-              validator: (text) {
-                if (text.isNotEmpty && text.length > 5) {
-                  return null;
-                } else {
-                  return '제대로 된 비밀번호를 입력해 주세요.';
-                }
-              },
-            ),
-            SizedBox(height: common_xs_gap),
-            TextFormField(
-              controller: _cpwController,
-              cursorColor: Colors.black54,
-              decoration: _textInputDecor('Confirm Password'),
-              validator: (text) {
-                if (text.isNotEmpty && _pwController.text == text) {
-                  return null;
-                } else {
-                  return '입력한 값이 비밀번호와 일치하지 않습니다.';
-                }
-              },
-            ),
-            FlatButton(
-              color: Colors.blue,
-              onPressed: () {
-                // 위 3개의 validator 가 전부 null을 반환하면 true가 오고 그렇지 않으면 false가 온다
-                if (_formkey.currentState.validate()) {
-                  print('Validation success!!');
-                }
-              },
-              child: Text(
-                'Join',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
+    return Scaffold(
+      //입력창이 밑쪽에 있어 키보드에 가려지는걸 방지하기 위해 키보드 위로 올라오도록 리사이징 한다
+      //resizeToAvoidBottomInset기능을 쓰기위해 Scaffold로 감싸줌
+      resizeToAvoidBottomInset: true,
+      body: Padding(
+        padding: const EdgeInsets.all(common_gap),
+        child: Form(
+          //스크롤이 가능한 ListView를 넣어줌
+          key: _formkey,
+          child: ListView(
+            children: [
+              SizedBox(
+                height: common_l_gap,
               ),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6)),
-            )
-          ],
+              Image.asset('assets/images/insta_text_logo.png'),
+              TextFormField(
+                controller: _emailController,
+                cursorColor: Colors.black54,
+                decoration: _textInputDecor('Email'),
+                validator: (text) {
+                  if (text.isNotEmpty && text.contains("@")) {
+                    return null;
+                  } else {
+                    return '정확한 이메일을 주소를 입력해주세요.';
+                  }
+                },
+              ),
+              SizedBox(height: common_xs_gap),
+              TextFormField(
+                controller: _pwController,
+                cursorColor: Colors.black54,
+                //비밀번호 암호화
+                obscureText: true,
+                decoration: _textInputDecor('Password'),
+                validator: (text) {
+                  if (text.isNotEmpty && text.length > 5) {
+                    return null;
+                  } else {
+                    return '제대로 된 비밀번호를 입력해 주세요.';
+                  }
+                },
+              ),
+              SizedBox(height: common_xs_gap),
+              TextFormField(
+                controller: _cpwController,
+                cursorColor: Colors.black54,
+                decoration: _textInputDecor('Confirm Password'),
+                validator: (text) {
+                  if (text.isNotEmpty && _pwController.text == text) {
+                    return null;
+                  } else {
+                    return '입력한 값이 비밀번호와 일치하지 않습니다.';
+                  }
+                },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  // 위 3개의 validator 가 전부 null을 반환하면 true가 오고 그렇지 않으면 false가 온다
+                  if (_formkey.currentState.validate()) {
+                    print('Validation success!!');
+                  }
+                },
+                child: Text(
+                  'Join',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6)),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -102,25 +107,29 @@ class _SignUpFormState extends State<SignUpForm> {
   InputDecoration _textInputDecor(String hint) {
     return InputDecoration(
       hintText: hint,
-      enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.grey[300],
-          ),
-          borderRadius: BorderRadius.circular(common_s_gap)),
-      errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.redAccent,
-          ),
-          borderRadius: BorderRadius.circular(common_s_gap)),
-
-      focusedErrorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.redAccent,
-          ),
-          borderRadius: BorderRadius.circular(common_s_gap)),
+      enabledBorder: _activeInputBorder(),
+      focusedBorder: _activeInputBorder(),
+      errorBorder: _errorInputBorder(),
+      focusedErrorBorder: _errorInputBorder(),
       //필드 색지정
       filled: true,
       fillColor: Colors.grey[100],
     );
+  }
+
+  OutlineInputBorder _errorInputBorder() {
+    return OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.redAccent,
+        ),
+        borderRadius: BorderRadius.circular(common_s_gap));
+  }
+
+  OutlineInputBorder _activeInputBorder() {
+    return OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.grey[300],
+        ),
+        borderRadius: BorderRadius.circular(common_s_gap));
   }
 }
