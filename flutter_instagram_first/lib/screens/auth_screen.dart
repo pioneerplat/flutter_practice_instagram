dc20f3011e5a9468342491abb299e6df4b076bbd
pioneerplat <pioneerplat@gmail.com>
2021-01-05 18:14:58 +0900

    37 richtext in auth page

--- flutter_instagram_first/lib/screens/auth_screen.dart
+++ flutter_instagram_first/lib/screens/auth_screen.dart
@@ -11,43 +11,53 @@ class AuthScreen extends StatefulWidget {
 
 //한가지 애니메이션을 사용할때 SingleTickerProviderStateMixin 사용한다.
 class _AuthScreenState extends State<AuthScreen> {
-
-
   int selectedForm = 0;
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            FadeStack(
-              selectedForm: selectedForm,
-            ),
-            Container(
-              child: FlatButton(
-                onPressed: () {
-                  setState(() {
-                    //if( <instance> is <class>){} 타입비교
-                    if (selectedForm == 0) {
-                      selectedForm = 1;
-                    } else {
-                      selectedForm = 0;
-                    }
-                  });
-
-                },
-                child: Text('go to Sign up'),
+        body: SafeArea(
+            child: Stack(
+      children: <Widget>[
+        FadeStack(
+          selectedForm: selectedForm,
+        ),
+        //Positioned Stack안에서만 사용 가능
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Container(
+            color: Colors.white,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  //if( <instance> is <class>){} 타입비교
+                  if (selectedForm == 0) {
+                    selectedForm = 1;
+                  } else {
+                    selectedForm = 0;
+                  }
+                });
+              },
+              child: RichText(
+                text: TextSpan(
+                  text: (selectedForm == 0)
+                      ? "Already have an account? "
+                      : "Don't have an account? ",
+                  style: TextStyle(color: Colors.grey),
+                  children: [
+                    TextSpan(
+                        text: (selectedForm == 0) ? "Sign In" : "Sign Up",
+                        style: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.bold)),
+                  ],
+                ),
               ),
             ),
-          ],
+          ),
         ),
-      ),
-    );
+      ],
+    )));
   }
 }
