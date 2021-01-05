e7ecc9a8826b831123a2d6d02a1ac9757aa8c8ac
pioneerplat <pioneerplat@gmail.com>
2021-01-05 22:17:16 +0900

    42 입력창 복사하자

--- flutter_instagram_first/lib/constants/common_size.dart
+++ flutter_instagram_first/lib/constants/common_size.dart
@@ -1,5 +1,8 @@
-const double common_gap = 14.0;
-const double common_xxs_gap = 8;
 const double common_l_gap = 16;
+const double common_gap = 14.0;
 const double common_s_gap = 12;
+const double common_xs_gap = 10;
+const double common_xxs_gap = 8;
+
+
 const double avatar_size = 30;
--- flutter_instagram_first/lib/widgets/sign_up_form.dart
+++ flutter_instagram_first/lib/widgets/sign_up_form.dart
@@ -12,10 +12,14 @@ class _SignUpFormState extends State<SignUpForm> {
 
   //TextFormField 위젯을 사용할 때 필요한 것
   TextEditingController _emailController = TextEditingController();
+  TextEditingController _pwController = TextEditingController();
+  TextEditingController _cpwController = TextEditingController();
 
   @override
   void dispose() {
     _emailController.dispose();
+    _pwController.dispose();
+    _cpwController.dispose();
     super.dispose();
   }
 
@@ -34,31 +38,56 @@ class _SignUpFormState extends State<SignUpForm> {
             Image.asset('assets/images/insta_text_logo.png'),
             TextFormField(
               controller: _emailController,
-              decoration: InputDecoration(
-                hintText: 'Email',
-                enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey[300],
-                    ),
-                    borderRadius: BorderRadius.circular(common_s_gap)),
-                //필드 색지정
-                filled: true,
-                fillColor: Colors.grey[100],
-              ),
-
-              validator: (text){
-                if(text.isNotEmpty && text.contains("@")){
+              decoration: _textInputDecor('Email'),
+              validator: (text) {
+                if (text.isNotEmpty && text.contains("@")) {
                   return null;
-                }else{
-                  return '정확한 이메일을 주소를 입력해주세요~';
+                } else {
+                  return '정확한 이메일을 주소를 입력해주세요.';
                 }
               },
             ),
-            TextFormField(),
-            TextFormField(),
+            SizedBox(height: common_xs_gap),
+            TextFormField(
+              controller: _pwController,
+              decoration: _textInputDecor('Password'),
+              validator: (text) {
+                if (text.isNotEmpty && text.length > 5) {
+                  return null;
+                } else {
+                  return '제대로 된 비밀번호를 입력해 주세요.';
+                }
+              },
+            ),
+            SizedBox(height: common_xs_gap),
+            TextFormField(
+              controller: _cpwController,
+              decoration: _textInputDecor('Confirm Password'),
+              validator: (text) {
+                if (text.isNotEmpty && _pwController.text == text) {
+                  return null;
+                } else {
+                  return '입력한 값이 비밀번호와 일치하지 않습니다.';
+                }
+              },
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  InputDecoration _textInputDecor(String hint) {
+    return InputDecoration(
+      hintText: hint,
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.grey[300],
+          ),
+          borderRadius: BorderRadius.circular(common_s_gap)),
+      //필드 색지정
+      filled: true,
+      fillColor: Colors.grey[100],
     );
   }
 }
