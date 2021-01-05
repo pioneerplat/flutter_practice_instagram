8d54bc929a0f0373f700ac99a4ed31f32d714d15
pioneerplat <pioneerplat@gmail.com>
2021-01-05 22:00:55 +0900

    41 TextFormField 사용해서 email input 만들자

--- flutter_instagram_first/lib/constants/common_size.dart
+++ flutter_instagram_first/lib/constants/common_size.dart
@@ -1,4 +1,5 @@
 const double common_gap = 14.0;
 const double common_xxs_gap = 8;
 const double common_l_gap = 16;
+const double common_s_gap = 12;
 const double avatar_size = 30;
--- flutter_instagram_first/lib/widgets/sign_up_form.dart
+++ flutter_instagram_first/lib/widgets/sign_up_form.dart
@@ -10,21 +10,54 @@ class _SignUpFormState extends State<SignUpForm> {
   //formkey란 form위젯의 상태를 저장하는 key
   GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
+  //TextFormField 위젯을 사용할 때 필요한 것
+  TextEditingController _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      //스크롤이 가능한 ListView를 넣어줌
-      key: _formkey,
-      child: ListView(
-        children: [
-          SizedBox(
-            height: common_l_gap,
-          ),
-          Image.asset('assets/images/insta_text_logo.png'),
-          TextFormField(),
-          TextFormField(),
-          TextFormField(),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(common_gap),
+      child: Form(
+        //스크롤이 가능한 ListView를 넣어줌
+        key: _formkey,
+        child: ListView(
+          children: [
+            SizedBox(
+              height: common_l_gap,
+            ),
+            Image.asset('assets/images/insta_text_logo.png'),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey[300],
+                    ),
+                    borderRadius: BorderRadius.circular(common_s_gap)),
+                //필드 색지정
+                filled: true,
+                fillColor: Colors.grey[100],
+              ),
+
+              validator: (text){
+                if(text.isNotEmpty && text.contains("@")){
+                  return null;
+                }else{
+                  return '정확한 이메일을 주소를 입력해주세요~';
+                }
+              },
+            ),
+            TextFormField(),
+            TextFormField(),
+          ],
+        ),
       ),
     );
   }
