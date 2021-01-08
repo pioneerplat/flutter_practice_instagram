f448646b4a8235e23b403813094d6352c8c9599c
pioneerplat <pioneerplat@gmail.com>
2021-01-08 23:08:54 +0900

    67 use multi provider

--- flutter_instagram_first/lib/screens/camera_screen.dart
+++ flutter_instagram_first/lib/screens/camera_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_first/models/camera_state.dart';
 import 'package:flutter_instagram_first/widgets/take_photo.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 class CameraScreen extends StatefulWidget {
+
+  CameraState _cameraState = CameraState();
   @override
   // PageView를 컨트롤하기 위한 컨트롤러
-  _CameraScreenState createState() => _CameraScreenState();
+  _CameraScreenState createState() {
+    _cameraState.getReadyToTakePhoto();
+    return _CameraScreenState();
+  }
 }
 
 class _CameraScreenState extends State<CameraScreen> {
@@ -20,51 +28,60 @@ class _CameraScreenState extends State<CameraScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_title),
-      ),
-      body: PageView(
-        controller: _pageController,
-        children: <Widget>[
-          Container(
-            color: Colors.cyanAccent,
-          ),
-          TakePhoto(),
-          Container(
-            color: Colors.greenAccent,
-          ),
-        ],
-        onPageChanged: (index) {
-          setState(() {
-            _currentIndex = index;
-            switch (_currentIndex) {
-              case 0:
-                _title = 'Gallery';
-                break;
-              case 1:
-                _title = 'Photo';
-                break;
-              case 2:
-                _title = 'Vedio';
-                break;
-            }
-          });
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        //아이콘을 안보이게 하기 위해
-        iconSize: 0,
-        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black54,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'GALLERY'),
-          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'PHOTO'),
-          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'VIDEO')
-        ],
-        currentIndex: _currentIndex,
-        onTap: _onItemTabbed,
+    return MultiProvider(
+      providers: [
+        //바로생성해서 던져주는 방법 과 value를 통해서 주는 방법 2가지가 있다
+        //여기서는 _cameraState.getReadyToTakePhoto();를 먼저 하기 위해서 밑에 방법을 사용한다
+        //ChangeNotifierProvider(create: (context) => CameraState())
+        ChangeNotifierProvider<CameraState>.value(value: widget._cameraState),
+
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_title),
+        ),
+        body: PageView(
+          controller: _pageController,
+          children: <Widget>[
+            Container(
+              color: Colors.cyanAccent,
+            ),
+            TakePhoto(),
+            Container(
+              color: Colors.greenAccent,
+            ),
+          ],
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+              switch (_currentIndex) {
+                case 0:
+                  _title = 'Gallery';
+                  break;
+                case 1:
+                  _title = 'Photo';
+                  break;
+                case 2:
+                  _title = 'Vedio';
+                  break;
+              }
+            });
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          //아이콘을 안보이게 하기 위해
+          iconSize: 0,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black54,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'GALLERY'),
+            BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'PHOTO'),
+            BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: 'VIDEO')
+          ],
+          currentIndex: _currentIndex,
+          onTap: _onItemTabbed,
+        ),
       ),
     );
   }
