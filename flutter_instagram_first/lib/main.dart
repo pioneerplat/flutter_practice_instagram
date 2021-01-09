70e3d57137dc312331db7d636558b21a48b67ea3
pioneerplat <pioneerplat@gmail.com>
2021-01-10 03:21:14 +0900

    firebase error 해결
    app/build.gradle 에 추가
    apply plugin: 'com.google.gms.google-services'
    
    main.dart 에 추가
    import 'package:firebase_core/firebase_core.dart';
    
    void main() async {
      WidgetsFlutterBinding.ensureInitialized();
      await Firebase.initializeApp();
      runApp(MyApp());
    }

--- flutter_instagram_first/lib/main.dart
+++ flutter_instagram_first/lib/main.dart
@@ -5,8 +5,11 @@ import 'package:flutter_instagram_first/models/firebase_auth_state.dart';
 import 'package:flutter_instagram_first/screens/auth_screen.dart';
 import 'package:flutter_instagram_first/widgets/my_progress_indicator.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
