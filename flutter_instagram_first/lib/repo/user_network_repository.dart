19e4abc87fd89f1f15a445988c47a2389c00adad
pioneerplat <pioneerplat@gmail.com>
2021-01-13 18:23:02 +0900

    105 create user model stream

--- flutter_instagram_first/lib/repo/user_network_repository.dart
+++ flutter_instagram_first/lib/repo/user_network_repository.dart
@@ -10,9 +10,17 @@ class UserNetworkRepository {
         .document(userKey);
 
     DocumentSnapshot snapshot = await userRef.get();
-    if(!snapshot.exists){
+    if (!snapshot.exists) {
       return await userRef.setData(UserModel.getMapForCreateUser(email));
     }
+  }
+
+  Stream<UserModel> getUserModelStream(String userKey) {
+    // .get()을 하면 1번만 가지고오고 ,
+    // .snapshots을 하면 stream을 보내주기 때문에 정보가 변할 때마다 보내준다.
+    return Firestore.instance.collection(COLLECTION_USERS)
+        .document(userKey)
+        .snapshots().transform(streamTransformer)
   }
 }
 
