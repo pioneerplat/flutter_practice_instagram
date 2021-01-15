8abebf7ab9b895f5dec39f9e5d8eb7f0bf3f0c33
pioneerplat <pioneerplat@gmail.com>
2021-01-15 21:57:23 +0900

    116 section button layout in share post

--- flutter_instagram_first/lib/screens/share_post_screen.dart
+++ flutter_instagram_first/lib/screens/share_post_screen.dart
@@ -27,23 +27,62 @@ class SharePostScreen extends StatelessWidget {
         ],
       ),
       body: ListView(
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.symmetric(vertical: common_gap, horizontal: common_gap),
-            leading: Image.file(
-              imageFile,
-              width: size.width / 6,
-              height: size.width / 6,
-              //사이즈가 오버되는 부분은 잘라준다
-              fit: BoxFit.cover,
-            ),
-            title: TextField(
-              decoration: InputDecoration(
-                  hintText: 'Write a caption...', border: InputBorder.none),
-            ),
-          ),
+        children: <Widget>[
+          _captionWithImage(),
+          //padding을 없애려면 thickness와 heignt를 똑같이 주면 된다
+          _divider,
+          _sectionButton('Tag People'),
+          _divider,
+          _sectionButton('Add Location'),
         ],
       ),
     );
   }
+
+/*
+  Divider _divider() {
+    return Divider(
+          color: Colors.grey[300],
+          thickness: 1,
+          height: 1,
+        );
+  }
+ */
+ // 이렇게 하면 호출할 때 _divider() 이 아닌 _divider로 이름만으로 호출할 수 있게 된다
+  Divider get _divider =>
+      Divider(
+        color: Colors.grey[300],
+        thickness: 1,
+        height: 1,
+      );
 }
+
+ListTile _sectionButton(String title) {
+  return ListTile(
+    title: Text(
+      title,
+      style: TextStyle(fontWeight: FontWeight.w400),
+    ),
+    trailing: Icon(Icons.navigate_next),
+    dense: true,
+    contentPadding: EdgeInsets.symmetric(horizontal: common_gap),
+  );
+}
+
+ListTile _captionWithImage() {
+  return ListTile(
+    contentPadding:
+    EdgeInsets.symmetric(vertical: common_gap, horizontal: common_gap),
+    leading: Image.file(
+      imageFile,
+      width: size.width / 6,
+      height: size.width / 6,
+      //사이즈가 오버되는 부분은 잘라준다
+      fit: BoxFit.cover,
+    ),
+    title: TextField(
+      decoration: InputDecoration(
+          hintText: 'Write a caption...', border: InputBorder.none),
+    ),
+  );
+}}
