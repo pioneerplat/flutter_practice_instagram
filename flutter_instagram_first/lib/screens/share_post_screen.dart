c6fd8070a62504566ead0555dd5f92dc65e04f28
pioneerplat <pioneerplat@gmail.com>
2021-01-15 22:27:21 +0900

    117 horizontal scroll tags

--- flutter_instagram_first/lib/screens/share_post_screen.dart
+++ flutter_instagram_first/lib/screens/share_post_screen.dart
@@ -2,13 +2,47 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_first/constants/common_size.dart';
 import 'package:flutter_instagram_first/constants/screen_size.dart';
+import 'package:flutter_tags/flutter_tags.dart';
 
 class SharePostScreen extends StatelessWidget {
   final File imageFile;
   final String postKey;
 
-  const SharePostScreen(this.imageFile, {Key key, @required this.postKey})
+  SharePostScreen(this.imageFile, {Key key, @required this.postKey})
       : super(key: key);
+
+  List<String> _tagItems = [
+    "approval",
+    "pigeon",
+    "brown",
+    "expenditure",
+    "compromise",
+    "citizen",
+    "inspire",
+    "relieve",
+    "grave",
+    "incredible",
+    "invasion",
+    "voucher",
+    "girl",
+    "relax",
+    "problem",
+    "queue",
+    "aviation",
+    "profile",
+    "palace",
+    "drive",
+    "money",
+    "revolutionary",
+    "string",
+    "detective",
+    "follow",
+    "text",
+    "bet",
+    "decade",
+    "means",
+    "gossip"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +68,23 @@ class SharePostScreen extends StatelessWidget {
           _sectionButton('Tag People'),
           _divider,
           _sectionButton('Add Location'),
+          Tags(
+            horizontalScroll: true,
+            itemCount: _tagItems.length,
+            //수평스크롤 높이
+            heightHorizontalScroll: 30,
+            itemBuilder: (index) => ItemTags(
+              index: index,
+              title: _tagItems[index],
+              activeColor: Colors.grey[200],
+              textActiveColor: Colors.black87,
+              borderRadius: BorderRadius.circular(4),
+              //splashColor: Colors.grey[800],
+              color: Colors.green,
+              //그림자
+              elevation: 2,
+            ),
+          )
         ],
       ),
     );
@@ -48,41 +99,40 @@ class SharePostScreen extends StatelessWidget {
         );
   }
  */
- // 이렇게 하면 호출할 때 _divider() 이 아닌 _divider로 이름만으로 호출할 수 있게 된다
-  Divider get _divider =>
-      Divider(
+  // 이렇게 하면 호출할 때 _divider() 이 아닌 _divider로 이름만으로 호출할 수 있게 된다
+  Divider get _divider => Divider(
         color: Colors.grey[300],
         thickness: 1,
         height: 1,
       );
-}
 
-ListTile _sectionButton(String title) {
-  return ListTile(
-    title: Text(
-      title,
-      style: TextStyle(fontWeight: FontWeight.w400),
-    ),
-    trailing: Icon(Icons.navigate_next),
-    dense: true,
-    contentPadding: EdgeInsets.symmetric(horizontal: common_gap),
-  );
-}
+  ListTile _sectionButton(String title) {
+    return ListTile(
+      title: Text(
+        title,
+        style: TextStyle(fontWeight: FontWeight.w400),
+      ),
+      trailing: Icon(Icons.navigate_next),
+      dense: true,
+      contentPadding: EdgeInsets.symmetric(horizontal: common_gap),
+    );
+  }
 
-ListTile _captionWithImage() {
-  return ListTile(
-    contentPadding:
-    EdgeInsets.symmetric(vertical: common_gap, horizontal: common_gap),
-    leading: Image.file(
-      imageFile,
-      width: size.width / 6,
-      height: size.width / 6,
-      //사이즈가 오버되는 부분은 잘라준다
-      fit: BoxFit.cover,
-    ),
-    title: TextField(
-      decoration: InputDecoration(
-          hintText: 'Write a caption...', border: InputBorder.none),
-    ),
-  );
-}}
+  ListTile _captionWithImage() {
+    return ListTile(
+      contentPadding:
+          EdgeInsets.symmetric(vertical: common_gap, horizontal: common_gap),
+      leading: Image.file(
+        imageFile,
+        width: size.width / 6,
+        height: size.width / 6,
+        //사이즈가 오버되는 부분은 잘라준다
+        fit: BoxFit.cover,
+      ),
+      title: TextField(
+        decoration: InputDecoration(
+            hintText: 'Write a caption...', border: InputBorder.none),
+      ),
+    );
+  }
+}
