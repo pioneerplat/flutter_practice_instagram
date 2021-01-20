2de3a5fdd0bd88a894de86d6660ec5cf56d093d1
pioneerplat <pioneerplat@gmail.com>
2021-01-21 02:46:48 +0900

    135 posts transformer

--- flutter_instagram_first/lib/repo/helper/transformers.dart
+++ flutter_instagram_first/lib/repo/helper/transformers.dart
@@ -2,34 +2,40 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_instagram_first/models/firestore/post_model.dart';
 import 'package:flutter_instagram_first/models/firestore/user_model.dart';
 
-class Transformers{
-
+class Transformers {
   final toUser = StreamTransformer<DocumentSnapshot, UserModel>.fromHandlers(
-    handleData: (snapshot, sink) async{
-      sink.add(UserModel.fromSnapshot(snapshot));
-    }
-  );
-  final toUsersExceptMe = StreamTransformer<QuerySnapshot, List<UserModel>>.fromHandlers(
-    handleData: (snapshot, sink) async{
-      int a = 1;
+      handleData: (snapshot, sink) async {
+    sink.add(UserModel.fromSnapshot(snapshot));
+  });
+  final toUsersExceptMe =
+      StreamTransformer<QuerySnapshot, List<UserModel>>.fromHandlers(
+          handleData: (snapshot, sink) async {
 
-      List<UserModel> users = [];
-      // 현재 유저(나)
-      FirebaseUser _firebaseUser = await FirebaseAuth.instance.currentUser();
-      snapshot.documents.forEach((documentSnapshot) {
-        //내 아이디를 제외한 유저들을 가지고 오기 위해
+    List<UserModel> users = [];
+    // 현재 유저(나)
+    FirebaseUser _firebaseUser = await FirebaseAuth.instance.currentUser();
+    snapshot.documents.forEach((documentSnapshot) {
+      //내 아이디를 제외한 유저들을 가지고 오기 위해
+      if (_firebaseUser.uid != documentSnapshot.documentID) {
+        //UserModel로 변경해서 list로 넣어준다
+        users.add(UserModel.fromSnapshot(documentSnapshot));
+      }
+    });
+    //QuerySnapshot이 도착할 때마다 유저리스트를 생성해서 싱크대를 통해서 나가게 되어있다.
+    sink.add(users);
+  });
+  final toPosts =
+      StreamTransformer<QuerySnapshot, List<PostModel>>.fromHandlers(
+          handleData: (snapshot, sink) async {
+    List<PostModel> posts = [];
 
-        if(_firebaseUser.uid != documentSnapshot.documentID) {
-          //UserModel로 변경해서 list로 넣어준다
-          a = a + 1;
-          users.add(UserModel.fromSnapshot(documentSnapshot));
-        }
-      });
-      //QuerySnapshot이 도착할 때마다 유저리스트를 생성해서 싱크대를 통해서 나가게 되어있다.
-      print(a);
-      sink.add(users);
-    }
-  );
+    snapshot.documents.forEach((documentSnapshot) {
+      posts.add(PostModel.fromSnapshot(documentSnapshot));
+    });
+    //QuerySnapshot이 도착할 때마다 유저리스트를 생성해서 싱크대를 통해서 나가게 되어있다.
+    sink.add(posts);
+  });
 }
--- flutter_instagram_first/lib/repo/post_network_repository.dart
+++ flutter_instagram_first/lib/repo/post_network_repository.dart
@@ -1,13 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_instagram_first/constants/firestore_keys.dart';
-
-class PostNetworkRepository {
+import 'package:flutter_instagram_first/models/firestore/post_model.dart';
+import 'package:flutter_instagram_first/repo/helper/transformers.dart';
+class PostNetworkRepository with Transformers {
   Future<Map<String, dynamic>> createNewPost(
       String postKey, Map<String, dynamic> postData) async {
     //해당 document의 reference 가져오기
     final DocumentReference postRef =
         Firestore.instance.collection(COLLECTION_POSTS).document(postKey);
-
 
     //reference를 통해서 snapshot을 가져옴
     final DocumentSnapshot postSnapshot = await postRef.get();
@@ -32,16 +32,20 @@ class PostNetworkRepository {
 
   Future<void> updatePostImageUrl({String postImg, String postKey}) async {
     final DocumentReference postRef =
-    Firestore.instance.collection(COLLECTION_POSTS).document(postKey);
+        Firestore.instance.collection(COLLECTION_POSTS).document(postKey);
     final DocumentSnapshot postSnapshot = await postRef.get();
 
     if (postSnapshot.exists) {
       await postRef.updateData({KEY_POSTIMG: postImg});
     }
   }
-  Stream<void> getPostsFromSpecificUser(String userKey) {
+
+  Stream<List<PostModel>> getPostsFromSpecificUser(String userKey) {
     // where은 컬렉션 안에서 where이하에 일치하는 부분만 가지고 오겠다
-    return Firestore.instance.collection(COLLECTION_POSTS).where(KEY_USERKEY, isEqualTo: userKey)
+    return Firestore.instance
+        .collection(COLLECTION_POSTS)
+        .where(KEY_USERKEY, isEqualTo: userKey)
+        .snapshots().transform(toPosts);
   }
 }
 
