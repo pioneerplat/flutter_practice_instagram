e33ea5af350727fcf8f71589ab333f2bc16c901c
pioneerplat <pioneerplat@gmail.com>
2021-01-21 04:59:07 +0900

    136 fetch all posts and combine them into one stream

--- flutter_instagram_first/lib/repo/helper/transformers.dart
+++ flutter_instagram_first/lib/repo/helper/transformers.dart
@@ -13,7 +13,6 @@ class Transformers {
   final toUsersExceptMe =
       StreamTransformer<QuerySnapshot, List<UserModel>>.fromHandlers(
           handleData: (snapshot, sink) async {
-
     List<UserModel> users = [];
     // 현재 유저(나)
     FirebaseUser _firebaseUser = await FirebaseAuth.instance.currentUser();
@@ -36,6 +35,28 @@ class Transformers {
       posts.add(PostModel.fromSnapshot(documentSnapshot));
     });
     //QuerySnapshot이 도착할 때마다 유저리스트를 생성해서 싱크대를 통해서 나가게 되어있다.
+    sink.add(posts);
+  });
+
+  final combineListOfPosts =
+      StreamTransformer<List<List<PostModel>>, List<PostModel>>.fromHandlers(
+          handleData: (listOfPosts, sink) async {
+    List<PostModel> posts = [];
+
+    //<List<List<PostModel>> 있는걸 다 꺼내서 하나의 리스트에 넣어준다
+    for(final postList in listOfPosts){
+      posts.addAll(postList);
+    }
+
+    //QuerySnapshot이 도착할 때마다 유저리스트를 생성해서 싱크대를 통해서 나가게 되어있다.
+    sink.add(posts);
+  });
+
+  final latestToTop =
+      StreamTransformer<List<PostModel>, List<PostModel>>.fromHandlers(
+          handleData: (posts, sink) async {
+    //비교한 값 중 큰 값을 위로 올림
+    posts.sort((a, b) => b.postTime.compareTo(a.postTime));
     sink.add(posts);
   });
 }
--- flutter_instagram_first/lib/repo/post_network_repository.dart
+++ flutter_instagram_first/lib/repo/post_network_repository.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_instagram_first/constants/firestore_keys.dart';
 import 'package:flutter_instagram_first/models/firestore/post_model.dart';
 import 'package:flutter_instagram_first/repo/helper/transformers.dart';
+import 'package:rxdart/rxdart.dart';
+//import 'package:rxdart/streams.dart';
+
 class PostNetworkRepository with Transformers {
   Future<Map<String, dynamic>> createNewPost(
       String postKey, Map<String, dynamic> postData) async {
@@ -45,7 +48,28 @@ class PostNetworkRepository with Transformers {
     return Firestore.instance
         .collection(COLLECTION_POSTS)
         .where(KEY_USERKEY, isEqualTo: userKey)
-        .snapshots().transform(toPosts);
+        .snapshots()
+        .transform(toPosts);
+  }
+
+  Stream<List<PostModel>> fetchPostsFromAllFollowers(List<dynamic> followers) {
+    final CollectionReference collectionReference =
+        Firestore.instance.collection(COLLECTION_POSTS);
+    List<Stream<List<PostModel>>> streams = [];
+    for (final follower in followers) {
+      //해당 followers에서 userkey가 매치되는 포스트들만 가지고 온다
+      streams.add(collectionReference
+          .where(KEY_USERKEY, isEqualTo: follower)
+          //스트림을 생성
+          .snapshots()
+          //스트림이 도착을 할 때 toPosts를 이용해서 QuerySnapshot을 List<PostModel>로 변경 해줌
+          .transform(toPosts));
+    }
+
+    //List<List<PostModel>> 이런 식으로 도착하는 걸 combineListOfPosts를 이용해
+    //하나의 리스트로 합쳐준 다음 다시 latestToTop을 사용해 순서를 정렬해서 stream으로 내보내 준다
+    return CombineLatestStream.list<List<PostModel>>(streams)
+        .transform(combineListOfPosts).transform(latestToTop);
   }
 }
 
