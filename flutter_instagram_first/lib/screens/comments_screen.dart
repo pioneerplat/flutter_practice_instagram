3c5ef884243747415c2f370359f1927535274eb8
pioneerplat <pioneerplat@gmail.com>
2021-01-21 23:32:31 +0900

    148 comments page input screen page

--- flutter_instagram_first/lib/screens/comments_screen.dart
+++ flutter_instagram_first/lib/screens/comments_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_first/constants/common_size.dart';
 
 class CommentsScreen extends StatefulWidget {
   @override
@@ -6,8 +7,53 @@ class CommentsScreen extends StatefulWidget {
 }
 
 class _CommentsScreenState extends State<CommentsScreen> {
+
+  TextEditingController _textEditingController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.amber,);
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Expanded(child: Container(color: Colors.amber)),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: common_gap),
+                    child: TextFormField(
+                      controller: _textEditingController,
+                      cursorColor: Colors.black54,
+                      decoration: InputDecoration(
+                        hintText: 'Add a comment...',
+                        border: InputBorder.none
+                      ),
+                      //textField에 적은 글을 value로 받아서 검사한다
+                      validator: (String value){
+                        if (value.isEmpty)
+                          return 'Input something';
+                        else
+                          return null;
+                      },
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: (){
+                    if(_formKey.currentState.validate()){
+                      //todo: push a comment
+                    }
+                  },
+                  child: Text("Post"),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
